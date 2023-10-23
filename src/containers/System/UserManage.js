266d8410b2c71bc02331627756795c929849a908
Minh Nhat Tran <thmm2512@gmail.com>
2023-10-23 10:22:26 +0700

    Design modal them moi nguoi dung

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,116 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss'
+import { getAllUsers, createNewUserService } from '../../services/userService'
+import ModalUser from './ModalUser';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            arrUsers: [],
+            isOpenModalUser: false
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        await this.getAllUsersFromReact()
     }
 
+    getAllUsersFromReact = async () => {
+        let response = await getAllUsers('ALL')
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+            // console.log('check state user', this.state.arrUsers)
+        }
+    }
+
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser
+        })
+    }
+    //properties / nested
+
+    createNewUser = async (data) => {
+        try {
+            let response = await createNewUserService(data)
+            if(response && response.errCode !== 0) {
+                alert(response.errMessage)
+            } else {
+                await this.getAllUsersFromReact()
+                this.setState({
+                    isOpenModalUser: false
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    /**Life cycle
+     * Run component
+     * 1. Run construct -> init state
+     * 2. Did mount
+     * 3. Render
+     */
 
     render() {
+        let arrUsers = this.state.arrUsers
+
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent = {this.toggleUserModal}
+                    createNewUser = {this.createNewUser}
+                />
+                <div className='title text-center'>Manage users</div>
+                <div className='mx-1 px-1'>
+                    <button className='btn btn-primary px-3' onClick={() => this.handleAddNewUser()}>
+                        <i className='fas fa-plus'></i> Add new users
+                    </button>
+                </div>
+
+                <div className='users-table-box mt-3 mx-1'>
+                    <table className='users-table'>
+                        <thead>
+                            <tr>
+                                <td>Email</td>
+                                <td>First name</td>
+                                <td>Last name</td>
+                                <td>Address</td>
+                                <td>Action</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {arrUsers && arrUsers.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className='fas fa-pencil-alt'></i></button>
+                                            <button className='btn-delete'><i className='fas fa-trash'></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,17 @@ const handleLogin = (email, password) => {
     return axios.post('/api/login', { email, password })
 }
 
+const getAllUsers = (inputId) => {
+    return axios.get(`/api/get-all-users?id=${inputId}`)
+}
+
+const createNewUserService = (data) => {
+    console.log('check data from service', data)
+    return axios.post('/api/create-new-user', data)
+}
+
 export {
     handleLogin,
+    getAllUsers,
+    createNewUserService,
  }
